c98cbce8677e4a217eb620912dcd0e2f04f767c3
acrisanto <alanvcrisanto@gmail.com>
2023-11-04 16:38:15 -0500

    add routes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT ?? 8000;
+
+const app = express();
+app.use(cors());
+app.use("/", require("./routes"));
+
+app.listen(PORT, () => {
+	console.log("Server running on port " + PORT);
+});

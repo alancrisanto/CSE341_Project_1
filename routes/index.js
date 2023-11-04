38311b87da6eebc2c1fec33f9d6224611a64b5cc
acrisanto <alanvcrisanto@gmail.com>
2023-11-04 18:50:48 -0500

    get routes working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+// router.get("/", (req, res) => {
+// 	res.send("Hello World!");
+// });
+
+router.use("/users", require("./usersRoute"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongodb = require("./data/database");
 
 const PORT = process.env.PORT ?? 8000;
 
@@ -8,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use("/", require("./routes"));
 
-app.listen(PORT, () => {
-	console.log("Server running on port " + PORT);
+mongodb.initDb((err) => {
+	if (err) {
+		console.error(err);
+	} else {
+		app.listen(PORT, () => {
+			console.log("Database connected and server running on port " + PORT);
+		});
+	}
 });
